23c7280725b5a5160def645acc3f939ab8563888
Florent CHAMFROY <florent.chamfroy@graviteesource.com>
2026-09-19 20:53:55 +0200

    build: drop what the parent already provides
    Plugin versions and the properties that fed them: the parent is the single
    place where they are decided, downgrades included.
    
    https://gravitee.atlassian.net/browse/BX-403

--- src/test/java/io/gravitee/policy/customqueryparametersparser/CustomQueryParametersParserPolicyV4IntegrationTest.java
+++ src/test/java/io/gravitee/policy/customqueryparametersparser/CustomQueryParametersParserPolicyV4IntegrationTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.gravitee.policy.customqueryparametersparser;/**
+package io.gravitee.policy.customqueryparametersparser; /**
  * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
